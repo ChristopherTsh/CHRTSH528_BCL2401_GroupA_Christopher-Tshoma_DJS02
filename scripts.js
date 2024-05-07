0478468b4610380a45603ce184780a188f793c33
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-05-07 16:15:06 +0200

    used floor math on my division, and made an if statement

--- scripts.js
+++ scripts.js
@@ -5,5 +5,10 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  result.innerText = Math.floor(dividend / divider);
+  
+  if( dividend === '' ||divider === '' ){
+    result.textContent = 'Division not performed. Both values are required in inputs. Try again'
+   }
+
 });
