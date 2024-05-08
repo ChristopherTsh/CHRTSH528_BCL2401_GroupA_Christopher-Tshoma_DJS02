7c7bbaf6e837d6ce3a8a497325b6b0efb124e91c
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-05-08 09:49:19 +0200

    using try to handle errors and catch to catch errors throw by specific if statement

--- scripts.js
+++ scripts.js
@@ -5,26 +5,29 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  
 
-  if (dividend === "" || divider === "") {
-    result.textContent =
-      "Division not performed. Both values are required in inputs. Try again";
-     
-  }else if (divider === 0) {
-    result.textContent =
-      "Division not performed. Invalid number provided. Try again";
-  }else if (isNaN(dividend) || isNaN(divider)) {
-    result.textContent =
-      "Something critical went wrong. Please reload the page";
-  } else {
-    result.innerText = Math.floor(dividend / divider);
+ 
+  try {
+    if (dividend === "" || divider === "") {
+      result.textContent =
+        "Division not performed. Both values are required in inputs. Try again";
+        throw new Error(
+          "Division not performed. Both values are required in inputs. Try again");
+         }else if (divider == 0) {
+      result.textContent =
+        "Division not performed. Invalid number provided. Try again";
+           throw new Error(
+        "Division not performed. Invalid number provided. Try again");
+    }else if (isNaN(dividend) || isNaN(divider)) {
+      result.textContent =
+        "Something critical went wrong. Please reload the page";
+        throw new Error(
+          "Something critical went wrong. Please reload the page");
+    } else {
+      result.innerText = Math.floor(dividend / divider);
+    }
+    
+  } catch (error) {
+    console.error("Error:", error.message);
   }
-  // try {
-  //   throw new Error(
-  //     "Division not performed. Invalid number provided. Try again"
-  //   );
-  // } catch (error) {
-  //   console.error("Error:", error.message);
-  // }
 });
