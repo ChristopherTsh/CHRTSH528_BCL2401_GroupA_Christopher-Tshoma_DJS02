5fce83daf81f663447fec5177b88314f9c23ec48
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-05-08 09:05:16 +0200

    using prettier , and debugging my if statement, and arranging them in order

--- scripts.js
+++ scripts.js
@@ -6,20 +6,28 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = Math.floor(dividend / divider);
-  
-  if( dividend === '' ||divider === '' ){
-    result.textContent = 'Division not performed. Both values are required in inputs. Try again'
-   }
 
-   if(divider === 0 ){
-    result.textContent = 'Division not performed. Invalid number provided. Try again'
+  if (dividend === "" || divider === "") {
+    result.textContent =
+      "Division not performed. Both values are required in inputs. Try again";
+  }
+
+  if (divider === 0) {
+    result.textContent =
+      "Division not performed. Invalid number provided. Try again";
+     }
+
+  if (typeof dividend !== "number" || typeof divider !== "number") {
+    result.textContent =
+      "Something critical went wrong. Please reload the page";
+  } else {
     
-    }try{
-      throw new Error('Division not performed. Invalid number provided. Try again')
-    }
-    catch(error){
-      console.error('Error:', error.message)
-    }
-   
-
+  }
+  try {
+    throw new Error(
+      "Division not performed. Invalid number provided. Try again"
+    );
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
 });
