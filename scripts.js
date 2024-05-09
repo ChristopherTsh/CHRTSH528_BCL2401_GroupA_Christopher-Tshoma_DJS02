a5fd7f3858decad94197be1804dfb8d3c23d85db
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-05-09 13:13:07 +0200

    debugging the spaces in the code , deleting un wanted new error , using class to manipulate css

--- scripts.js
+++ scripts.js
@@ -6,28 +6,26 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
- 
   try {
     if (dividend === "" || divider === "") {
       result.textContent =
         "Division not performed. Both values are required in inputs. Try again";
-        throw new Error(
-          "Division not performed. Both values are required in inputs. Try again");
-         }else if (divider == 0) {
+        return;
+    }
+    if (dividend >= 0 && divider == 0) {
       result.textContent =
         "Division not performed. Invalid number provided. Try again";
-           throw new Error(
-        "Division not performed. Invalid number provided. Try again");
-    }else if (isNaN(dividend) || isNaN(divider)) {
-      result.textContent =
-        "Something critical went wrong. Please reload the page";
-        throw new Error(
-          "Something critical went wrong. Please reload the page");
-    } else {
-      result.innerText = Math.floor(dividend / divider);
+      throw new Error(
+        "Division not performed. Invalid number provided. Try again"
+      );
     }
-    
+    if (isNaN(dividend) || isNaN(divider)) {
+      document.body.innerHTML =
+        "<div class='critical-error'>Something critical went wrong. Please reload the page.</div>";
+      throw new Error("Something critical went wrong. Please reload the page");
+    }
+    result.innerText = Math.floor(dividend / divider);
   } catch (error) {
-    console.error("Error:", error.message);
+    console.error(error.stack);
   }
 });
